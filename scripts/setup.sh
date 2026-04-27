cfa351c34b8445f49ee650b10815305416319172
Timothy Stout <testerbomb2014@gmail.com>
2026-04-27 14:45:27 -0400

    Update patch line in setup script from 209 to 205

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,8 +4,8 @@ set -e
 # Configure
 cmake ../cmake -D PKG_OPENMP=yes -D BUILD_OMP=yes
 
-# Patch line 209
-sed -i '209s/#pragma omp parallel for LMP_DEFAULT_NONE LMP_SHARED(grad_u) schedule(static)/#pragma omp parallel for LMP_DEFAULT_NONE LMP_SHARED(grad_u, xmid) schedule(static)/' ../src/OPENMP/fix_nvt_sllod_omp.cpp
+# Patch line 205
+sed -i '205s/#pragma omp parallel for LMP_DEFAULT_NONE LMP_SHARED(grad_u) schedule(static)/#pragma omp parallel for LMP_DEFAULT_NONE LMP_SHARED(grad_u, xmid) schedule(static)/' ../src/OPENMP/fix_nvt_sllod_omp.cpp
 echo "xmid patch applied"
 # Build
 cmake --build . -- -j4
